82340c5ca8dd9a053d8843e90ad5cce5a6ccfa75
Luca <luca.camillini@studio.unibo.it>
2023-11-21 15:22:13 +0100

    ...

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -1,41 +1,73 @@
 package it.unibo.deathnote.impl;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import it.unibo.deathnote.api.DeathNote;
-import java.lang.Exception;
+
+import static it.unibo.deathnote.api.DeathNote.RULES;
+
 
 public class DeathNoteImpl implements DeathNote{
+
+    Map<String, DeathInformation> deaths = new LinkedHashMap<>();
+    private String name;
+
     @Override
-    public String getDeathCause(String name){
-        throw new UnsupportedOperationException();
+    public String getDeathCause(final String name){
+        return deaths.get(name).deathCause;
     }
 
     @Override
-    public String getDeathDetails(String name){
-        throw new UnsupportedOperationException();
+    public String getDeathDetails(final String name){
+        return deaths.get(name).deathDetails;
     }
 
     @Override
     public String getRule(int ruleNumber) {
-        throw new IllegalArgumentException();
+        Assertions.
+        return RULES.get(ruleNumber);
     }
 
     @Override
     public boolean isNameWritten(String name){
-        throw new UnsupportedOperationException();
+        return !this.name.isEmpty();
     }
 
     @Override
     public boolean writeDeathCause(String cause){
-        throw new UnsupportedOperationException();
+        deaths.get(this.name).setDeathCause(cause);
+        if (deaths.get(this.name).deathCause.isEmpty()) {
+            return false;
+        }
+        return true;
+
     }
 
     @Override
-    public boolean writeDetails(String details){
-        throw new UnsupportedOperationException();
+    public boolean writeDetails(final String details){
+        deaths.get(this.name).setDeathDetails(details);
+        if (deaths.get(this.name).deathDetails.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public void writeName(String name){
-        throw new UnsupportedOperationException();
+    public void writeName(final String name){
+        this.name = name;
+    }
+
+    public class DeathInformation{
+        private String deathCause;
+        private String deathDetails;
+
+        private void setDeathCause(final String cause){
+
+        }
+
+        private void setDeathDetails(final String details){
+        }
+
     }
 }
--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -4,9 +4,12 @@ import it.unibo.deathnote.api.DeathNote;
 import it.unibo.deathnote.impl.DeathNoteImpl;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static java.lang.Thread.sleep;
 
 public class TestDeathNote {
 
@@ -20,34 +23,73 @@ public class TestDeathNote {
 
     @Test
     public void TestIllegalRule(){
-
-        for ( int i = 1 ; i < DeathNote.RULES.size() ; i++) {
-            try {
-                deathNote.getRule(i);
-            } catch (Exception e) {
-                Assertions.assertEquals(IllegalArgumentException.class , e.getClass() ); //controllo che sia un IllegalArgumentException
-                Assertions.assertFalse(deathNote.getRule(i).isEmpty());
-            }
+        int numRule = -1; 
+        try {
+            deathNote.getRule(numRule);
+            Assertions.fail("IllegalArgoument but do not throw Exception for numRule=" + numRule);
+            numRule++;
+            deathNote.getRule(numRule);
+            Assertions.fail("IllegalArgoument but do not throw Exception for numRule=" + numRule);
+        }catch (Exception e) {
+            Assertions.assertEquals(IllegalArgumentException.class , e.getClass() ); //controllo che sia un IllegalArgumentException
+            Assertions.assertFalse(deathNote.getRule(numRule).isEmpty());
         }
         
+    
     }
 
     @Test
     public void humanDeath(){
-        
-        Assertions.assertFalse(deathNote.isNameWritten("Mattia"));
-        deathNote.writeName("Mattia");
-        Assertions.assertTrue(deathNote.isNameWritten("Mattia"));
+        /* verify that the human has not been written in the notebook yet*/
+        Assertions.assertFalse(deathNote.isNameWritten("Alex Biondi"));
+        /* write the human in the notebook */
+        deathNote.writeName("Alex Biondi");
+        /* verify that the human has been written in the notebook */
+        Assertions.assertTrue(deathNote.isNameWritten("Alex Biondi"));
+        /* verify that another human has not been written in the notebook */
         Assertions.assertFalse(deathNote.isNameWritten("UNALTRONOME"));
+        /* verify that the empty string has not been written in the notebook */
         Assertions.assertFalse(deathNote.isNameWritten(""));
     }
 
-    @Test
-    public void humanDeathTimeout(){
 
-        deathNote.writeDetails("")
+    @Test
+    public void testDeathCauseTimeout() throws InterruptedException{
+
+        try {
+            deathNote.getDeathCause("death cause");
+            Assertions.fail("IllegalStateException but do not throw Exception");
+        } catch (Exception e) {
+            Assertions.assertEquals(IllegalStateException.class , e);
+        }
+        deathNote.writeName("Mattia Morri");
+        Assertions.assertEquals("heart attack", deathNote.getDeathCause("Mattia Morri"));
+        deathNote.writeName("Llazar Tafa");
+        deathNote.writeDeathCause("karting accident");
+        Assertions.assertEquals("karting accident", deathNote.getDeathCause("Llazar Tafa"));
+        sleep(100);
+        deathNote.writeDeathCause("Changed death cause");
+        Assertions.assertEquals("karting accident", deathNote.getDeathCause("Llazar Tafa"));
     }
 
+    @Test
+    public void testDeathDetailsTimeout() throws InterruptedException{
+        try {
+            deathNote.getDeathCause("death details");
+            Assertions.fail("IllegalStateException but do not throw Exception");
+        } catch (Exception e) {
+            Assertions.assertEquals(IllegalStateException.class , e);
+        }
+        deathNote.writeName("Jacopo Mosconi");
+        Assertions.assertEquals("", deathNote.getDeathDetails("Jacopo Mosconi"));
+        deathNote.writeDetails("ran for too long");
+        Assertions.assertEquals("ran for too long", deathNote.getDeathCause("Jacopo Mosconi"));
+        deathNote.writeName("Simone Lizzo");
+        sleep(6100);
+        deathNote.writeDetails("bit by a spider while sleeping");
+        Assertions.assertEquals("", deathNote.getDeathCause("Simone Lizzo"));
+
+    }
 
 
 
